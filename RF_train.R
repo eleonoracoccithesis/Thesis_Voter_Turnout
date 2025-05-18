8af741da78b54730e93d131edb76ca4bf40b7107
eleonoracoccithesis <e.cocci@tilburguniversity.edu>
2025-05-18 15:44:47 +0200

    REF_train

--- RF_train.R
+++ RF_train.R
@@ -3,12 +3,14 @@ library(caret)
 library(dplyr)
 library(MLmetrics)
 
+
 #1 PREPARE BINARY TRAINING SET__________________________________________________
 train_df_bin <- train_final %>%
   filter(vote_2023 %in% c(1, 2)) %>%
   select(-participant, -year, -split)
 
 train_df_bin$vote_2023 <- factor(ifelse(train_df_bin$vote_2023 == 1, "Yes", "No"), levels = c("No", "Yes"))
+
 
 #2 DEFINE CUSTOM F1 FUNCTION____________________________________________________
 f1Summary <- function(data, lev = NULL, model = NULL) {
@@ -17,6 +19,7 @@ f1Summary <- function(data, lev = NULL, model = NULL) {
   f1 <- F1_Score(y_pred = data$pred, y_true = data$obs, positive = "Yes")
   c(Precision = precision, Recall = recall, F1 = f1)
 }
+
 
 #3 CONTROL SETTIGNS WITH BAGGIN + DOWNSAMPLING__________________________________
 cv_control_ensemble <- trainControl(
@@ -28,6 +31,7 @@ cv_control_ensemble <- trainControl(
   summaryFunction = f1Summary
 )
 
+
 #4 TRAIN ENSEMBLE RANDOM FOREST_________________________________________________
 set.seed(42)
 ensemble_rf <- train(
@@ -38,6 +42,7 @@ ensemble_rf <- train(
   trControl = cv_control_ensemble,
   metric = "F1"
 )
+
 
 #5 REVIEW PERFORMANCE___________________________________________________________
 print(ensemble_rf)
